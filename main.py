6a1535bd804d455241c81b6b284900151566e77b
07Nurcahyo <bagusnurcahyo4@gmail.com>
2024-07-29 12:38:24 +0700

    Proses absensi dengan cara menampilkan wajah yang terdapat dalam folder Images di camera

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import face_recognition
 import os
+from datetime import datetime
 
 
 # Mendeteksi file yang ada di dalam folder Images
@@ -25,22 +26,37 @@ def cariEncoding(gambar):
         encode = face_recognition.face_encodings(img)[0]
         encodeList.append(encode)
     return encodeList
-
 listEncoding = cariEncoding(gambar)
 print('Jumlah : ', len(listEncoding))
 print('Proses Encoding Selesai!')
+
+
+# Fungsi absensi
+def absensi(nama):
+    with open('Kehadiran.csv', 'r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if nama not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{nama},{dtString}')
+
+
+# Fungsi untuk menutup kamera
+def tutupKamera():
+    kamera.release()
+    cv2.destroyAllWindows()
+    exit()
 
 
 # Pencarian wajah
 kamera = cv2.VideoCapture(0)
 kamera.set(cv2.CAP_PROP_FRAME_WIDTH, 1092)
 kamera.set(cv2.CAP_PROP_FRAME_HEIGHT, 1024)
-
-def tutupKamera():
-    kamera.release()
-    cv2.destroyAllWindows()
-    exit()
-
+# Perulangan untuk mendeteksi wajah
 while True:
     success, img = kamera.read()
     imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
@@ -61,8 +77,8 @@ while True:
             y1, x2, y2, x1 = faceLoc
             y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.rectangle(img, (x1, y2-35), (x2, y2), (0, 255, 0), cv2.FILLED)
-            cv2.putText(img, nama, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            cv2.putText(img, 'Nama : '+nama, (x1+6, y1-6), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 2)
+            absensi(nama)
 
     cv2.imshow('Detektor Wajah', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
